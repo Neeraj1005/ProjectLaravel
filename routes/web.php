8f1d49fce40f788287ec72161ff28ebdc44f1ed7
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-26 19:43:04 +0530

    Simple Route
    Simple route create

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('user/{id?}', function ($id='No id is available') {
-    return view('user', ['data'=>$id]);
-    //return view('user', ['data'=>'peter parker']);
+Route::get('user', function () {
+//Route::get('user/{id?}', function ($id='No id is available') {
+    //return view('user', ['data'=>$id]);
+    return view('user', ['data'=>'peter parker']);
 });
