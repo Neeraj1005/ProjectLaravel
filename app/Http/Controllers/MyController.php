3883d30789176513c12478f073168273d3d1d560
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-26 20:48:03 +0530

    simple controller
    Simple controller

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    //
+    public function index($id){
+    	
+    	return ['id'=>$id, 'user'=>'peter parker', 'email'=>'parker@mail.com'];
+    	//echo "heloo this is controller".$id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('user', function () {
+/*Route::get('user', function () {
 //Route::get('user/{id?}', function ($id='No id is available') {
     //return view('user', ['data'=>$id]);
     return view('user', ['data'=>'peter parker']);
-});
+});*/
+
+Route::get('mycontroller/{id}','MyController@index');
